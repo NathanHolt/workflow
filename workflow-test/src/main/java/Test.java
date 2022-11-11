049dda4bfee68816f86288e74f573e3372a56e2a
Nathan Holt <69161294+NathanHolt@users.noreply.github.com>
2022-11-10 19:51:24 -0500

    added threads

--- workflow-test/src/main/java/Test.java
+++ workflow-test/src/main/java/Test.java
@@ -1,2 +1,37 @@
 public class Test {
+
+
+    public static void main(String[] args) {
+        RunnyBoi test = new RunnyBoi();
+
+        test.start();
+
+        System.out.println("number 1");
+
+        System.out.println(RunnyBoi.num);
+
+        long counter = 1;
+        for (int i = 0; i < RunnyBoi.num * 9999999; i++) {
+            counter = counter + counter;
+        }
+
+        System.out.println(RunnyBoi.num);
+    }
+}
+
+class RunnyBoi extends Thread {
+    static int num = 5;
+
+    public void run() {
+        System.out.println("number 2");
+
+        long counter = 1;
+        for (int i = 0; i < RunnyBoi.num * 9999999; i++) {
+            counter = counter + counter;
+        }
+
+        System.out.println("number 3");
+
+        num = num * 25;
+    }
 }
