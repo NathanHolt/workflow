52f4a557df4fe995c25066862e16b2a637a98dfd
Nathan Holt <69161294+NathanHolt@users.noreply.github.com>
2022-11-10 20:07:26 -0500

    two threads too many

--- workflow-test/src/main/java/Test.java
+++ workflow-test/src/main/java/Test.java
@@ -1,36 +1,50 @@
 public class Test {
 
 
-    public static void main(String[] args) {
-        RunnyBoi test = new RunnyBoi();
+    public static void main(String[] args) throws InterruptedException {
+        RunnyBoi test1 = new RunnyBoi(1);
+        RunnyBoi test2 = new RunnyBoi(2);
 
-        test.start();
+        test1.start();
+        test2.start();
+
+        try {
+            test1.sleep(10000);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
 
         System.out.println("number 1");
 
-        System.out.println(RunnyBoi.num);
 
         long counter = 1;
-        for (int i = 0; i < RunnyBoi.num * 9999999; i++) {
+        for (int i = 0; i < 100 * 9999999; i++) {
             counter = counter + counter;
         }
 
-        System.out.println(RunnyBoi.num);
+        System.out.println(test1.num);
+        System.out.println(test2.num);
+
     }
 }
 
 class RunnyBoi extends Thread {
-    static int num = 5;
+    public int num = 5;
+
+    public RunnyBoi(int i) {
+        num = i;
+    }
 
     public void run() {
-        System.out.println("number 2");
+        System.out.println("number 2 " + num);
 
         long counter = 1;
-        for (int i = 0; i < RunnyBoi.num * 9999999; i++) {
+        for (int i = 0; i < 100 * 9999999; i++) {
             counter = counter + counter;
         }
 
-        System.out.println("number 3");
+        System.out.println("number 3 " + num);
 
         num = num * 25;
     }
